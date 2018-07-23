4da42bae28cb5860baaee6cdc41ba6b5c5ff2058
Reice Griffin <reicelovesllamas@gmail.com>
2018-07-23 18:47:21 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -2,7 +2,7 @@
 # Challenge 1: Fill this string in with your favorite word
 
 your_favorite_word = "llamas"
-puts "My favorite word is #{llamas}
+puts "My favorite word is your_favorite_word'
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
